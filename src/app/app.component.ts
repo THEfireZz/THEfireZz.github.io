035905345f32ae96a28e154fb6d3d69d5d7ab41a
THEfireZz <maximedhaeyere@gmail.com>
2023-12-26 00:19:47 +0100

    Add router links and routes for About Me page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 
 import { HeaderComponent } from './components/header/header.component';
@@ -9,7 +9,7 @@ import { FooterComponent } from './components/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent,FooterComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
