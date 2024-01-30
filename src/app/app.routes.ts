e5a57fe6e43fbf2134f111ea1b7270f218575c0f
THEfireZz <maximedhaeyere@gmail.com>
2024-01-30 12:17:26 +0100

    Add formation component and update app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { AboutMeComponent } from './components/pages/about-me/about-me.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { FormationComponent } from './components/pages/formation/formation.component';
 
 
 
 export const routes: Routes = [
     { path: 'about-me', component: AboutMeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'formation', component: FormationComponent },
+
+    { path: '**', pathMatch: 'full', redirectTo: 'home' },
 
 ];
