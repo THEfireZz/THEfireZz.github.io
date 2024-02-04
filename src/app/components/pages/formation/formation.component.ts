0ea27480046b702be44b4f92a895352166cc5b7b
THEfireZz <maximedhaeyere@gmail.com>
2024-02-04 15:16:04 +0100

    Update formation component to use a list

--- src/app/components/pages/formation/formation.component.ts
+++ src/app/components/pages/formation/formation.component.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-formation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './formation.component.html',
   styleUrl: './formation.component.css'
 })
 export class FormationComponent {
   launch = 'assets/images/launch.svg'
   
+
+  formations = [
+    {
+      date: '2024',
+      title: 'Fin de l\'alternance chez Stellantis',
+      description: 'Description 1',
+    },
+    {
+      date: '2023 - 2024',
+      title: '3 ème année de BUT Informatique',
+      description: 'Description 1',
+    },
+    {
+      date: '2022',
+      title: 'Début de l\'alternance chez Stellantis',
+      description: 'Description 1',
+    },
+    {
+      date: '2022 - 2023',
+      title: '2 ème année de BUT Informatique',
+      description: 'Description 1',
+    },
+    {
+      date: '2021 - 2022',
+      title: '1 er année de BUT Informatique',
+      description: 'Description 1',
+    },
+    {
+      date: '2020 - 2021',
+      title: 'Baccalauréat général',
+      description: 'Description 2',
+    },
+  ]
 }
