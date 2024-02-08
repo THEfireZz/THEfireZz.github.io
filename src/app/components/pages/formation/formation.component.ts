e4665cf81aec23c847cf35a6bd5dbcaa2d1c0abd
THEfireZz <maximedhaeyere@gmail.com>
2024-02-08 12:33:35 +0100

    start of apprenticeship and 2nd BUT year at the same level

--- src/app/components/pages/formation/formation.component.ts
+++ src/app/components/pages/formation/formation.component.ts
@@ -17,31 +17,37 @@ export class FormationComponent {
       date: '2024',
       title: 'Fin de l\'alternance chez Stellantis',
       description: 'Description 1',
+      isInGroupe: false
     },
     {
       date: '2023 - 2024',
       title: '3 ème année de BUT Informatique',
       description: 'Description 1',
+      isInGroupe: false
     },
     {
       date: '2022',
       title: 'Début de l\'alternance chez Stellantis',
       description: 'Description 1',
+      isInGroupe: true
     },
     {
       date: '2022 - 2023',
       title: '2 ème année de BUT Informatique',
       description: 'Description 1',
+      isInGroupe: false
     },
     {
       date: '2021 - 2022',
       title: '1 er année de BUT Informatique',
       description: 'Description 1',
+      isInGroupe: false
     },
     {
       date: '2020 - 2021',
       title: 'Baccalauréat général',
       description: 'Description 2',
+      isInGroupe: false
     },
   ]
 }
